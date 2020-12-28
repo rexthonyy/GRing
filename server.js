71c8a945ad3c3289121b360ccf677e6dd38f59e0
rexthonyy <rexthonyy@gmail.com>
2020-12-28 16:39:51 +0100

    Made dotenv not to be used in production environments

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
-require('dotenv').config();
+// require('dotenv').config();
+if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 
 const fs = require('fs');
 const express = require('express');
